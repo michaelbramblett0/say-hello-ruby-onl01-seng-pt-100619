714ceed3eebd857291d034b76a6860c5b276dbb3
mystical-quotation-3156 <michaelbramblett0@gmail.com>
2019-10-13 21:36:11 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
-def say_hello(name,language="Ruby")
-  puts "Hello #{name} Ruby Programmer!"
+def say_hello(name="Ruby Programmer!")
+  puts "Hello #{name}"
 end
 
-say_hello("Mitchell", "Python")
+say_hello("Mitchell")
